7b3c4350d71b54f58f46ce5084ba242d323ca411
SFarida <faridagueleu@gmail.com>
2023-04-07 16:35:13 +0100

    Update initialize parameter

--- my_list.rb
+++ my_list.rb
@@ -3,7 +3,7 @@ require_relative 'my_enumerable'
 class MyList
   include MyEnumerable
 
-  # attr_accessor :list
+  attr_accessor :list
 
   def initialize(*list)
     @list = list
@@ -11,10 +11,5 @@ class MyList
 
   def each(&block)
     @list.each(&block)
-    # puts @list
   end
 end
-
-list = MyList.new(1, 2, 3, 4)
-puts list.all? {|e| e < 5}
-puts list.all? {|e| e > 5}
